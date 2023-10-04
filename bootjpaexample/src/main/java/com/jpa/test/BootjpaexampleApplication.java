85866002bf9828d8eba92ea065446178de45c041
Rahul <rahulkr.vk@gmail.com>
2023-10-04 23:48:39 +0530

    inserting to table

--- bootjpaexample/src/main/java/com/jpa/test/BootjpaexampleApplication.java
+++ bootjpaexample/src/main/java/com/jpa/test/BootjpaexampleApplication.java
@@ -2,12 +2,27 @@ package com.jpa.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.jpa.test.dao.UserRepository;
+import com.jpa.test.entities.User;
 
 @SpringBootApplication
 public class BootjpaexampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootjpaexampleApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(BootjpaexampleApplication.class, args);
+	
+		UserRepository bean = context.getBean(UserRepository.class);
+		
+		User user = new User();
+		user.setName("Rahul");
+		user.setCity("Delhi");
+		user.setStatus("I am java developer");
+
+		User user1 = bean.save(user);
+		System.out.println(user1);
+		
 	}
 
 }
