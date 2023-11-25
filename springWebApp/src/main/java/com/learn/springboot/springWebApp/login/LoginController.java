6a668e3bf63c60ba9828acf87ebd1cbfef3d59be
Rahul <rahulkr.vk@gmail.com>
2023-11-25 17:03:41 +0530

    logincontroller & login jsp

---
+++ springWebApp/src/main/java/com/learn/springboot/springWebApp/login/LoginController.java
@@ -0,0 +1,14 @@
+package com.learn.springboot.springWebApp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping("login")
+	public String gotoLoginPage() {
+		return "login";
+	}
+	
+}
---
+++ springWebApp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -0,0 +1,6 @@
+<html>
+	<head>
+		<title>Login Page</title>
+	</head>
+	<body>Welcome to the login page !</body>
+</html>
