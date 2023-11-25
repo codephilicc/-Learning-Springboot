0a7c14f1431672a96fecb44d00efa595081aecfc
Rahul <rahulkr.vk@gmail.com>
2023-11-25 15:53:37 +0530

    say-hello

---
+++ springWebApp/src/main/java/com/learn/springboot/springWebApp/hello/SayHelloController.java
@@ -0,0 +1,16 @@
+package com.learn.springboot.springWebApp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+	@RequestMapping("say-hello")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello!What are you learning today?";
+	}
+	
+}
