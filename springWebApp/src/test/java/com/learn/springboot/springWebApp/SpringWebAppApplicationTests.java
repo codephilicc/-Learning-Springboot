f5095c52e316b301c5afb1f0bd73580a70458dd3
Rahul <rahulkr.vk@gmail.com>
2023-11-25 15:40:13 +0530

    first web app, in 28 minutes

---
+++ springWebApp/src/test/java/com/learn/springboot/springWebApp/SpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springboot.springWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
