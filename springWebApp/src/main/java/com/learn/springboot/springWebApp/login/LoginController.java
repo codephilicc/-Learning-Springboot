708b0c98e6d8dee9801ebee3288e4e3816dbc8cc
Rahul <rahulkr.vk@gmail.com>
2023-11-26 11:03:15 +0530

    Adding loggerFactory

--- springWebApp/src/main/java/com/learn/springboot/springWebApp/login/LoginController.java
+++ springWebApp/src/main/java/com/learn/springboot/springWebApp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.learn.springboot.springWebApp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@RequestMapping("login")
 	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
 		model.put("name",name);
+		
+		logger.debug("Request param is {}", name);
+		logger.info("I want this printed at info level");
+		logger.warn("I want this printed at warn level");
 		System.out.println("Request param is "+name);
 		return "login";
 	}
