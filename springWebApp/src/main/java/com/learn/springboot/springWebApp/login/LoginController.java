c33d988011e3dfcedb67e3242247a51d67ee6e53
Rahul <rahulkr.vk@gmail.com>
2023-12-03 20:00:09 +0530

    using welcome.jsp

--- springWebApp/src/main/java/com/learn/springboot/springWebApp/login/LoginController.java
+++ springWebApp/src/main/java/com/learn/springboot/springWebApp/login/LoginController.java
@@ -1,22 +1,27 @@
 package com.learn.springboot.springWebApp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
 //	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
-	@RequestMapping("login")
+	@RequestMapping(value="login",method=RequestMethod.GET )
 	public String gotoLoginPage() {
-//		model.put("name",name);
-//		
-//		logger.debug("Request param is {}", name);
-//		logger.info("I want this printed at info level");
-//		logger.warn("I want this printed at warn level");
-//		System.out.println("Request param is "+name);
 		return "login";
 	}
+	
+	@RequestMapping(value="login",method=RequestMethod.POST )
+	public String gotoWelcomePage(@RequestParam String name,@RequestParam String password,ModelMap model) {
+		model.put("name",name);
+		model.put("password",password);
+		return "welcome";
+	}
+	
 	
 }
---
+++ springWebApp/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>Welcome Page</title>
+	</head>
+	<body>
+	<div>Welcome to Springboot</div>
+	<div>Name is: ${name}</div>
+	<div>Password is: ${password}</div>
+	
+	
+	
+	</body>
+</html>
