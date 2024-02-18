acdb4c9277a194c90fbb0e62b1dc4a585fe9ff1b
Rahul <rahulkr.vk@gmail.com>
2024-02-18 09:08:38 +0530

    upload method

--- restfileprocess/src/main/java/com/learn/rest/fileprocessing/controllers/FileController.java
+++ restfileprocess/src/main/java/com/learn/rest/fileprocessing/controllers/FileController.java
@@ -1,5 +1,22 @@
 package com.learn.rest.fileprocessing.controllers;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
 public class FileController {
+
+	private String UPLOAD_DIR = "/Users/Dell/Documents/Uploads/";
+
+	@PostMapping("/upload")
+	public boolean upload(@RequestParam("file") MultipartFile file) throws IllegalStateException, IOException {
+		file.transferTo(new File(UPLOAD_DIR + file.getOriginalFilename()));
+		return true;
+	}
 
 }
