b2438a20437f85339fa2fd2da9ac3df5f407fc98
Rahul <rahulkr.vk@gmail.com>
2024-03-22 19:41:53 +0530

    springrest

---
+++ springrest/springrest/src/main/java/com/example/springrest/DemoProjectForSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectForSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectForSpringBootApplication.class, args);
+	}
+
+}
